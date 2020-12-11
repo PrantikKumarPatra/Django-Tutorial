2da409895884bed5e502504f46f8dec5204d3740
PrantikKumarPatra <prantikpatra2015@gmail.com>
2020-12-11 11:03:06 +0530

    Third Page  and if else statement added

--- motion/urls.py
+++ motion/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('',views.index,name='index'),
-    path('mysecondpage',views.mysecondpage,name='mysecondpage')
+    path('mysecondpage',views.mysecondpage,name='mysecondpage'),
+    path('mythirdpage',views.mythirdpage,name='mythirdpage')
 ]
--- motion/views.py
+++ motion/views.py
@@ -7,3 +7,22 @@ def index(request):
 
 def mysecondpage(request):
     return render(request,'second.html')
+
+def mythirdpage(request):
+    var = "Hello World"
+    greeting="hey how are you"
+    fruits=['apple','mango','banana']
+    num1,num2=3,5
+    ans=num1>num2
+    #print(ans)
+    mydictionary={
+        "var" : var,
+        "msg" : greeting,
+        "myfruits": fruits,
+        "num1":num1,
+        "num2":num2,
+
+        "ans":ans
+    }
+    return render(request,'third.html',context=mydictionary)
+
